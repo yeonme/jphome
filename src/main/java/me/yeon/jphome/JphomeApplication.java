17c435a129c7027a7d48d65906123fb066b33db8
yeonme <yeon@yeon.me>
2018-10-30 00:15:41 +0900

    initial commit

---
+++ src/main/java/me/yeon/jphome/JphomeApplication.java
@@ -0,0 +1,12 @@
+package me.yeon.jphome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JphomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JphomeApplication.class, args);
+	}
+}
---
+++ src/test/java/me/yeon/jphome/JphomeApplicationTests.java
@@ -0,0 +1,16 @@
+package me.yeon.jphome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JphomeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
